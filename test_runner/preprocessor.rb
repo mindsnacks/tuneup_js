09104eb20a82de6dd1d36eb4c61c80945f8d76ff
Javier Soto <javier.api@gmail.com>
2013-01-23 14:38:12 -0800

    Added support for placeholders in imports. Fix for when the same file is imported with a different relative path.

--- test_runner/preprocessor.rb
+++ test_runner/preprocessor.rb
@@ -1,21 +1,25 @@
 class Preprocessor
-  def self.process(file)
-    self.process_imports(file, [])
+  def self.process(file, app_name)
+    self.process_imports(file, app_name, [])
   end
   
 private
   IMPORT_STATEMENT = /#import "([^"]+)"/
-  def self.process_imports(file, imported_file_names)
+  BUNDLE_ID_PLACEHOLDER = /\{bundle_id\}/
+  def self.process_imports(file, app_name, imported_file_names)
     content = File.read(file)
+
     content.gsub(IMPORT_STATEMENT) do
-      next if imported_file_names.include? $1
-      
-      imported_file_names << $1
-      import_file = File.join(File.dirname(file), $1)
+      parsed_file_name = $1.gsub(BUNDLE_ID_PLACEHOLDER, app_name)
+      import_file = File.join(File.dirname(file), parsed_file_name)
+
+      next if imported_file_names.include? import_file
+      imported_file_names << import_file
+
       begin
-        "// begin #{File.basename($1)}" << "\n" << 
-        process_imports(import_file, imported_file_names) << "\n" <<
-        "// end #{File.basename($1)}" << "\n"
+        "// begin #{File.basename(parsed_file_name)}" << "\n" <<
+        process_imports(import_file, app_name, imported_file_names) << "\n" <<
+        "// end #{File.basename(parsed_file_name)}" << "\n"
       rescue Exception => e
         STDERR.puts "Unable to process file #{import_file}: #{e}"
         $&
